fcac8df7939f378b53b8a148579fcef61483681f
janvi67 <janviraychura7890@gmail.com>
2024-12-18 09:12:54 +0530

    commiit

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,43 +1,4 @@
-// import {
-//   Card,
-//   Page,
-//   Layout,
-//   TextContainer,
-//   Image,
-//   Stack,
-//   Link,
-//   Heading,
-// } from "@shopify/polaris";
-// import { TitleBar } from "@shopify/app-bridge-react";
 
-// import { ProductCard, ProductsCard } from "../components";
-// import { useAppQuery } from "../hooks";
-// import { ProductList } from "../components/ProductList";
-
-// export default function HomePage() {
-//   const { data, isLoading, refetch, isRefetching } = useAppQuery({
-//     url: "/api/products",
-//   });
-
-//   console.log("data: ", data);
-
-//   return (
-//     <Page title="Dashboard">
-//       <Layout>
-//         <Layout.Section>
-//           <ProductsCard />
-//         </Layout.Section>
-//         <Layout.Section>
-//           <ProductList
-//             data={data}
-//             isLoading={isLoading}
-//             isRefetching={isRefetching}
-//           />
-//         </Layout.Section>
-//       </Layout>
-//     </Page>
-//   );
-// }
 
 import {
   Card,
